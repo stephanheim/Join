e7370650e4f32169e80832151bfd859c9b1036c0
Marzzzo <marco.burdick@web.de>
2025-01-28 21:23:39 +0100

    add Title Privacy Policy and Legal Notice

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const pageTitles = {
-    add_task: 'Add Task',
-    summary: 'Summary User',
-    board: 'Board',
-    contacts: 'Contacts'
+  add_task: 'Add Task',
+  summary: 'Summary User',
+  board: 'Board',
+  contacts: 'Contacts',
+  privacy_policy: 'Privacy Policy',
+  legal_notice: 'Legal notice',
 };
 
-
 async function loadPageContentPath(page) {
-    let contentPages = document.getElementById('content');
-    const content = await fetchContent(`${page}.html`);
-    contentPages.innerHTML = content; 
-    changePageTitles(page);  
+  let contentPages = document.getElementById('content');
+  const content = await fetchContent(`${page}.html`);
+  contentPages.innerHTML = content;
+  changePageTitles(page);
 }
-
 
 async function fetchContent(page) {
-    try {
-        const response = await fetch(`../pages/${page}`);
-        return await response.text(); 
-    } catch (error) {
-        console.error('Fehler beim Abrufen'); 
-    }
+  try {
+    const response = await fetch(`../pages/${page}`);
+    return await response.text();
+  } catch (error) {
+    console.error('Fehler beim Abrufen');
+  }
 }
 
-
 function changePageTitles(page) {
-    let changeTitles = pageTitles[page]; 
-    document.title = changeTitles; 
+  let changeTitles = pageTitles[page];
+  document.title = changeTitles;
 }
