c1942281d18a0d2f72ca9e90307523989e66b19b
paskas <pascal.kasbeitzer@gmail.com>
2025-01-28 21:07:26 +0100

    delete js.doc

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/**
- * Ein Objekt, das den Titel für jede Seite enthält.
- * @type {Object.<string, string>}
- */
 const pageTitles = {
     add_task: 'Add Task',
     summary: 'Summary User',
@@ -10,12 +6,6 @@ const pageTitles = {
 };
 
 
-/**
- * Lädt den Inhalt einer Seite und setzt den Titel des Dokuments entsprechend der Seite.
- * 
- * @param {string} page - Der Name der Seite, die geladen werden soll.
- * @returns {Promise<void>} Ein Promise, das den Abschluss des Ladevorgangs anzeigt.
- */
 async function loadPageContentPath(page) {
     let contentPages = document.getElementById('content');
     const content = await fetchContent(`${page}.html`);
@@ -24,12 +14,6 @@ async function loadPageContentPath(page) {
 }
 
 
-/**
- * Ruft den Inhalt einer Seite ab.
- * 
- * @param {string} page - Der Name der Seite, deren Inhalt geladen werden soll.
- * @returns {Promise<string>} Der Inhalt der Seite als Text.
- */
 async function fetchContent(page) {
     try {
         const response = await fetch(`../pages/${page}`);
@@ -40,11 +24,6 @@ async function fetchContent(page) {
 }
 
 
-/**
- * Ändert den Titel des Dokuments entsprechend dem übergebenen Seitenparameter.
- * 
- * @param {string} page - Der Name der Seite, dessen Titel gesetzt werden soll.
- */
 function changePageTitles(page) {
     let changeTitles = pageTitles[page]; 
     document.title = changeTitles; 
