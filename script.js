162aba36f3aaac327e450245831b2f433cd5f72e
stephanheim <stephanheim21@gmail.com>
2025-02-13 23:43:29 +0100

    fertigstellung contact-floater

--- script.js
+++ script.js
@@ -1,100 +1,100 @@
+let contactOpen = false;
+
 const pageTitles = {
-  add_task: 'Add Task',
-  summary: 'Summary User',
-  board: 'Board',
-  contacts: 'Contacts',
-  privacy_policy: 'Privacy Policy',
-  legal_notice: 'Legal notice',
-  help: 'Help',
+  add_task: "Add Task",
+  summary: "Summary User",
+  board: "Board",
+  contacts: "Contacts",
+  privacy_policy: "Privacy Policy",
+  legal_notice: "Legal notice",
+  help: "Help",
 };
 
-
 async function loadPageContentPath(page) {
-  let contentPages = document.getElementById('content');
+  let contentPages = document.getElementById("content");
   const content = await fetchContent(`${page}.html`);
   contentPages.innerHTML = content;
   changePageTitles(page);
   hideSubMenu();
 }
 
-
 async function fetchContent(page) {
   try {
     const response = await fetch(`../pages/${page}`);
     return await response.text();
   } catch (error) {
-    console.error('Fehler beim Abrufen');
+    console.error("Fehler beim Abrufen");
   }
 }
-
 
 function changePageTitles(page) {
   let changeTitles = pageTitles[page];
   document.title = changeTitles;
 }
 
-
 function openAddFloatingTask() {
-  let addTask = document.getElementById('floatingAddTask');
+  let addTask = document.getElementById("floatingAddTask");
   addTask.innerHTML = addTaskTemplate();
-  document.body.style.overflow = 'hidden';
-  addTask.classList.remove('slideOut');
-  addTask.classList.add('slideIn');
-  addTask.classList.remove('d-none');
+  document.body.style.overflow = "hidden";
+  addTask.classList.remove("slideOut");
+  addTask.classList.add("slideIn");
+  addTask.classList.remove("d-none");
   setTimeout(() => {
-    addTask.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+    addTask.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
   }, 200);
 }
 
-
 function closeAddFloatingTask() {
-  let floatingTask = document.getElementById('floatingAddTask');
-  floatingTask.classList.remove('slideIn');
-  floatingTask.classList.add('slideOut');
-  floatingTask.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  let floatingTask = document.getElementById("floatingAddTask");
+  floatingTask.classList.remove("slideIn");
+  floatingTask.classList.add("slideOut");
+  floatingTask.style.backgroundColor = "rgba(0, 0, 0, 0)";
   setTimeout(() => {
-    floatingTask.classList.add('d-none');
-    floatingTask.innerHTML = '';
-    document.body.style.overflow = '';
+    floatingTask.classList.add("d-none");
+    floatingTask.innerHTML = "";
+    document.body.style.overflow = "";
   }, 100);
 }
 
-
 function toggleSubMenu() {
-  const submenu = document.getElementById('submenu');
-  submenu.classList.toggle('d-none');
+  const submenu = document.getElementById("submenu");
+  submenu.classList.toggle("d-none");
 }
 
-
-function hideSubMenu(){
-  let submenu = document.getElementById('submenu');
+function hideSubMenu() {
+  let submenu = document.getElementById("submenu");
   if (submenu) {
-    submenu.classList.add('d-none');
+    submenu.classList.add("d-none");
   }
 }
 
+function overlayClick(event) {
+  let overlayContent = document.getElementById("contactFloater");
+  if (!overlayContent.contains(event.target)) {
+    closeNewContact();
+  }
+}
 
 function addNewContact() {
-  let addContact = document.getElementById('addContactOverlay');
+  let addContact = document.getElementById("addContactOverlay");
   addContact.innerHTML = generateFloaterHTML();
-  document.body.style.overflow = 'hidden';
-  addContact.classList.remove('slideOut');
-  addContact.classList.add('slideIn');
-  addContact.classList.remove('d-none');
+  document.body.style.overflow = "hidden";
+  addContact.classList.remove("slideOut");
+  addContact.classList.add("slideIn");
+  addContact.classList.remove("d-none");
   setTimeout(() => {
-    addContact.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+    addContact.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
   }, 200);
 }
 
-
 function closeNewContact() {
-  let closeFloater = document.getElementById('addContactOverlay');
-  closeFloater.classList.remove('slideIn');
-  closeFloater.classList.add('slideOut');
-  closeFloater.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  let closeFloater = document.getElementById("addContactOverlay");
+  closeFloater.classList.remove("slideIn");
+  closeFloater.classList.add("slideOut");
+  closeFloater.style.backgroundColor = "rgba(0, 0, 0, 0)";
   setTimeout(() => {
-    closeFloater.classList.add('d-none');
-    closeFloater.innerHTML = '';
-    document.body.style.overflow = '';
+    closeFloater.classList.add("d-none");
+    closeFloater.innerHTML = "";
+    document.body.style.overflow = "";
   }, 100);
 }
