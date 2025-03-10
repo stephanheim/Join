03a0e0cee2c96adf31245cc45c72b9137d6e5228
stephanheim <stephanheim21@gmail.com>
2025-03-10 22:38:49 +0100

    Validierung implementiert, alle console.log gelöscht

--- js/components/add_contact_temp.js
+++ js/components/add_contact_temp.js
@@ -1,59 +1,59 @@
 function generateFloaterHTML() {
   return `
-<form id="contactForm" onsubmit="return validateForm();">
+<form id="contactForm" onsubmit="return validateForm()">
 <div id="contactFloater" class="main-floater">
-<div class="add-contact">
-  <img src="../assets/img/join.svg" class="add-icon" />
-  <div class="add-text">
-    <span class="add-title">Add contact</span>
-    <span class="add-subtitle">Tasks are better with a team!</span>
-  </div>
-  <div class="add-seperator"></div>
-</div>
-<div class="add-form">
-  <div class="add-form-circle">
-    <img src="../assets/icons/person-cnt.svg" class="add-icon-pers" />
-  </div>
-  <div class="right-wrap">
-     <div onclick="closeNewContact()" class="add-close-div">
-       <img src="../assets/icons/close.svg" class="add-close-btn" />
+  <div class="add-contact">
+    <img src="../assets/img/join.svg" class="add-icon" />
+    <div class="add-text">
+      <span class="add-title">Add contact</span>
+      <span class="add-subtitle">Tasks are better with a team!</span>
     </div>
-    <div class="add-fields">
-      <div class="input-div">
-        <input type="text" placeholder="Name" class="add-input" id="addContName" minlength="2" maxlength="50" required />
-        <img src="../assets/icons/person_input.svg" class="input-img-person" />
-      </div>
-      <div class="input-div">
-        <input type="email" placeholder="Email" class="add-input" id="addContMail" required />
-        <img src="../assets/icons/mail.svg" class="input-img-mail" />
-      </div>
-      <div class="input-div">
-        <input type="tel" placeholder="Phone" class="add-input" id="addContPhone" required  />
-        <img src="../assets/icons/call.svg" class="input-img-call" />
-      </div>
+    <div class="add-seperator"></div>
+  </div>
+  <div class="add-form">
+    <div class="add-form-circle">
+      <img src="../assets/icons/person-cnt.svg" class="add-icon-pers" />
     </div>
-    <div class="add-btn-div">
-      <button onclick="closeNewContact() "class="add-btn-cancel">
-        Cancel
-        <div class="btn-icons">
-          <img src="../assets/icons/close.svg" class="img-btn-close" />
+    <div class="right-wrap">
+       <div onclick="closeNewContact()" class="add-close-div">
+         <img src="../assets/icons/close.svg" class="add-close-btn" />
+      </div>
+      <div class="add-fields">
+        <div class="input-div">
+          <input type="text" placeholder="Name" class="add-input" id="addContName" minlength="2" maxlength="50" required />
+          <img src="../assets/icons/person_input.svg" class="input-img-person" />
         </div>
-      </button>
-      <button type="submit" class="add-btn-create">
-        Create contact
-        <div class="btn-icons">
-          <img src="../assets/icons/check.svg" class="img-btn-check" />
+        <div class="input-div">
+          <input type="email" placeholder="Email" class="add-input" id="addContMail" required />
+          <img src="../assets/icons/mail.svg" class="input-img-mail" />
         </div>
-      </button>
+        <div class="input-div">
+          <input type="tel" placeholder="Phone" class="add-input" id="addContPhone" required />
+          <img src="../assets/icons/call.svg" class="input-img-call" />
+        </div>
+      </div>
+      <div class="add-btn-div">
+        <button onclick="closeNewContact()" class="add-btn-cancel">
+          Cancel
+          <div class="btn-icons">
+            <img src="../assets/icons/close.svg" class="img-btn-close" />
+          </div>
+        </button>
+        <button type="submit" class="add-btn-create">
+          Create contact
+          <div class="btn-icons">
+            <img src="../assets/icons/check.svg" class="img-btn-check" />
+          </div>
+        </button>
+      </div>
     </div>
   </div>
-</div>
 </div>
 </form>
 `;
 }
 
-function generateGroupHTML (group) {
+function generateGroupHTML(group) {
   return `
     <div class="cnt-letter-div">
           <span class="cnt-letter">${group}</span>
