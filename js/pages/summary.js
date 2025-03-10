b62294d69e4e706d70caf26f4c320acf5ccd1bd5
paskas <pascal.kasbeitzer@gmail.com>
2025-03-10 20:29:19 +0100

    summary css updated setActiveToBoard added ensures board change class active

--- js/pages/summary.js
+++ js/pages/summary.js
@@ -1,3 +1,17 @@
-function initSummary(){
+function initSummary() {
   loadContactsFromFirebase();
+}
+
+
+function setActiveToBoard() {
+  const toBoard = document.getElementById('sumToBoard');
+  const removeSum = document.getElementById('removeActive');
+  if (toBoard) {
+    toBoard.classList.add('active');
+    toBoard.style.pointerEvents = 'none';
+  }
+  if (removeSum) {
+    removeSum.classList.remove('active');
+    removeSum.style.pointerEvents = 'auto';
+  }
 }
